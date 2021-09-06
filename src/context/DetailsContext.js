8c4ab6069b22f2420b0ef3002621a97aae49d4a4
L-David-26 <davidlez2mgk@gmail.com>
2021-09-06 12:36:10 -0500

    main

--- src/context/DetailsContext.js
+++ src/context/DetailsContext.js
@@ -1,24 +1,8 @@
 import React, { useState } from "react";
 
-export const DetailsContext = React.createContext({
+const DetailsContext = React.createContext({
   name: "",
   state: true,
 });
 
-export const api = () => {
-  const res = fetch(``);
-  const data = res.json();
-  this.Profile__context = data;
-};
 
-export const principall = () => {
-  const les = "git__bash";
-
-  const ctx = useState();
-  return (
-    <div>
-      {les}
-      {ctx}
-    </div>
-  );
-};
